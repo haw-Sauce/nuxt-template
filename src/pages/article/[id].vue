3ebf80abbab19f06a77159d3d38d29d7e9337f2c
山楂 <10665560+haw_gc@user.noreply.gitee.com>
2024-12-23 17:15:00 +0800

    feat: 添加路由中间件

---
+++ src/pages/article/[id].vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+definePageMeta({
+    middleware: 'auth',
+})
+
+const route = useRoute();
+
+console.log(route)
+</script>
+
+<template>
+  <div class="article-page">article</div>
+</template>
+
+<style lang="scss" scoped></style>
